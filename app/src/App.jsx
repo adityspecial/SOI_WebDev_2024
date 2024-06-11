33f54fbb21d87a2e9ba720796352300b830c9ea2
Aditya kumar <145295567+adityspecial@users.noreply.github.com>
2024-06-11 09:47:30 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import { Navbar } from './components/Navbar'
 
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import About from './components/About';
+import Services from './components/Services';
+import Contacts from './components/Contacts';
+import "./index.css"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;

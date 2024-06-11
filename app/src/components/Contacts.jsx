63915b8c9c4d80c6f01ccb58463dd0f13b63d483
Aditya kumar <145295567+adityspecial@users.noreply.github.com>
2024-06-11 09:44:27 +0530

    Add files via upload

---
+++ app/src/components/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto p-4 mt-20">
+      <h1 className="text-3xl font-semibold">contacts</h1>
+      <p className="mt-4 gap-10">hello.</p>
+    </div>
+  )
+}
+
+export default Contacts

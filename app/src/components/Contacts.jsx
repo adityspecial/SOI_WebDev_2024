3215f171d594b964374b18b13688f628b736bcb8
adityspecial <145295567+adityspecial@users.noreply.github.com>
2024-06-13 14:06:57 +0530

    updated contact page

--- app/src/components/Contacts.jsx
+++ app/src/components/Contacts.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-
+// src/components/Contacts.jsx
+import React from 'react';
+import './Contacts.css';
+import Footer from "./Footer"
 const Contacts = () => {
   return (
-    <div className="max-w-screen-xl mx-auto p-4 mt-20">
-      <h1 className="text-3xl font-semibold">contacts</h1>
-      <p className="mt-4 gap-10">hello.</p>
+    <div className="container mx-auto p-4 flex flex-col items-center space-y-6 pt-24">
+      <h2 className="text-3xl font-semibold text-center mb-6">Contact Us</h2>
+      <div className="w-full flex flex-col md:flex-row items-center md:justify-around space-y-6 md:space-y-0">
+        <div className="w-full md:w-1/2 p-4">
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">Name</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">Message</label>
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="message"
+                placeholder="Message"
+                rows="4"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >Send</button>
+            </div>
+          </form>
+        </div>
+      
+        
+      </div><div className='w-full'>   <Footer/></div>
+     
     </div>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;

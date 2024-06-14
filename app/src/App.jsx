9c616699f5be625e5726e863d3440e3bf35d2c34
adityspecial <145295567+adityspecial@users.noreply.github.com>
2024-06-14 19:49:57 +0530

    update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,27 @@
-
+// App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
 import About from './components/About';
 import Services from './components/Services';
 import Contacts from './components/Contacts';
-import "./index.css"
+import Signup from './components/Signup';
+import Login from './components/Login';
+import './index.css';
+import Home from "./components/Home"
+
 function App() {
+
   return (
     <Router>
       <Navbar />
       <Routes>
+      <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contacts" element={<Contacts />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );

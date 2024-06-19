2724c09eaabb417e14ce2f738819e19bb2e0e5d0
Aditya kumar <145295567+adityspecial@users.noreply.github.com>
2024-06-19 23:50:10 +0530

    Update Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,10 +1,25 @@
-const mongoose =require("mongoose")
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true }
+});
 
-const UserModel = mongoose.model("users", UserSchema)
-module.exports = UserModel
+UserSchema.pre('save', async function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function(password) {
+  const user = this;
+  return await bcrypt.compare(password, user.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;

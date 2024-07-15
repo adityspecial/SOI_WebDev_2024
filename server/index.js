2acbe587a6bc69ac2a22e6a12226eb85151ebd0a
Harsh Chauhan <73985490+harz05@users.noreply.github.com>
2024-07-15 12:58:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: [""],
+    origin: ["https://soi-web-dev-2024-frontend.vercel.app"],
     methods: ["POST","GET"],
     credentials: true
   }
